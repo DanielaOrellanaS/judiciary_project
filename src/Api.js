e37b26031dfe272f8d3df53a4859e7bb9f634217
DanielaOrellanaS <deorellana@espe.edu.ec>
2024-07-19 01:49:36 -0500

    change on data

---
+++ src/Api.js
@@ -0,0 +1,46 @@
+const apiUrl = 'https://rest-api-notification.onrender.com';
+const localUrl = 'http://127.0.0.1:8000';  
+
+async function getJudgment() {
+  try {
+    const response = await fetch(`${apiUrl}/judgment/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch judgments');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching judgments:', error);
+    throw error;
+  }
+}
+
+async function saveJudgment(data) {
+  try {
+    const response = await fetch(`${apiUrl}/judgment/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Failed to save judgment data');
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error('Error saving judgment:', error);
+    throw error;
+  }
+}
+
+export { getJudgment, saveJudgment }
--- src/components/RequireDataComponent.js
+++ src/components/RequireDataComponent.js
@@ -1,46 +1,162 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../components.css';
+import { getJudgment, saveJudgment } from '../Api';
 
 const RequireDataComponent = () => {
+  const [formData, setFormData] = useState({
+    numJudgment: '',
+    adjudicated: '',
+    judge: '',
+    identificationType: '',
+    identification: '',
+    name: '',
+  });
+
+  const [judgmentData, setJudgmentData] = useState([]);
+  
+  const getCurrentDate = () => {
+    const date = new Date();
+    return date.toISOString().split('T')[0]; 
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getJudgment();
+        setJudgmentData(data);
+      } catch (error) {
+        console.error('Error fetching judgment:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await saveJudgment(formData);
+      console.log('Judgment saved successfully:', response);
+    } catch (error) {
+      console.error('Error saving judgment:', error);
+    }
+  };
+
   return (
     <div className="transaction-container">
-      <form className="transaction-form">
+      <form className="transaction-form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="juzgado">Juzgado:</label>
-          <input type="text" id="juzgado" name="juzgado" />
+          <label htmlFor="adjudicated">Juzgado:</label>
+          <input
+            type="text"
+            id="adjudicated"
+            name="adjudicated"
+            value={formData.adjudicated}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="juez">Juez:</label>
-          <input type="text" id="juez" name="juez" />
+          <label htmlFor="judge">Juez:</label>
+          <input
+            type="text"
+            id="judge"
+            name="judge"
+            value={formData.judge}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="oficio">Oficio No:</label>
-          <input type="text" id="oficio" name="oficio" />
+          <input
+            type="text"
+            id="oficio"
+            name="oficio"
+            value={Math.floor(1000 + Math.random() * 9000).toString()} 
+            readOnly 
+          />
         </div>
         <div className="form-group">
           <label htmlFor="mail">Mail:</label>
-          <input type="email" id="mail" name="mail" />
+          <input
+            type="email"
+            id="mail"
+            name="mail"
+            value={"jsegalla@cnj.gob.ec"}
+            readOnly 
+          />
         </div>
         <div className="form-group">
           <label htmlFor="cargo">Cargo:</label>
-          <input type="text" id="cargo" name="cargo" />
+          <input
+            type="text"
+            id="cargo"
+            name="cargo"
+            value={"Secretario"}
+            readOnly
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="juicio">Juicio:</label>
-          <input type="text" id="juicio" name="juicio" />
+          <label htmlFor="numJudgment">Juicio:</label>
+          <input
+            type="text"
+            id="numJudgment"
+            name="numJudgment"
+            value={formData.numJudgment}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="datosAccionado">Datos accionado:</label>
-          <input type="text" id="datosAccionado" name="datosAccionado" />
+          <label htmlFor="name">Datos accionado:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="ciDemandante">CI Demandante:</label>
-          <input type="text" id="ciDemandante" name="ciDemandante" />
+          <label htmlFor="identificationType">Tipo Identificacion:</label>
+          <select
+            id="identificationType"
+            name="identificationType"
+            value={formData.identificationType}
+            onChange={handleInputChange}
+          >
+            <option value="cedula">Cédula</option>
+            <option value="pasaporte">Pasaporte</option>
+            <option value="ruc">RUC</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="identification">Identificacion Demandante:</label>
+          <input
+            type="text"
+            id="identification"
+            name="identification"
+            value={formData.identification}
+            onChange={handleInputChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="fecha">Fecha:</label>
-          <input type="date" id="fecha" name="fecha" />
+          <input
+            type="date"
+            id="fecha"
+            name="fecha"
+            value={getCurrentDate()}
+            readOnly
+          />
         </div>
+        <button type="submit">Guardar</button>
       </form>
       <div className="table-header">
         <h2>Cuentas Ahorros-Corrientes</h2>
@@ -60,17 +176,19 @@ const RequireDataComponent = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-            <td>Ejemplo</td>
-          </tr>
+          {judgmentData.map((item) => (
+            <tr key={item.idJudgment}>
+              <td>{item.name}</td>
+              <td>{item.lastname}</td>
+              <td>{item.identificationType}</td>
+              <td>{item.identification}</td>
+              <td>{item.accountType}</td>
+              <td>{item.bank}</td>
+              <td>{item.accountNumber}</td>
+              <td>{item.accountStatus}</td>
+              <td>{item.responseDate}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
