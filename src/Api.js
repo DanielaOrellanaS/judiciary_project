3c5c1bd4278a64df9b7fbbbe30dce806ce66dd37
DanielaOrellanaS <deorellana@espe.edu.ec>
2024-08-07 02:21:59 -0500

    modify url for api

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,5 @@
-const localUrl = 'https://rest-api-notification.onrender.com';
-const apiUrl = 'http://127.0.0.1:8000';  
+const localUrl = 'http://127.0.0.1:8000';
+const apiUrl = 'https://rest-api-notification.onrender.com';  
 
 export async function getJudgment() {
   try {
